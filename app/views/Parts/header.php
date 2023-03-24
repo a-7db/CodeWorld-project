d203c31d617abd54b99a8ede9046cb9c51040873
a-7db <Ahmed97874@gmail.com>
2023-03-24 16:19:53 +0300

    header & footer config

---
+++ app/views/Parts/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/HomePage/bootstrap.min.css">
+    <link rel="stylesheet" href="css/HomePage/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/HomePage/HomePage.css">
+
+
+    <title>Hello, world!</title>
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="70">
