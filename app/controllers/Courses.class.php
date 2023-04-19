153899165113c3f9713ee3446853bbb251a3a643
MohammedAlhayki <123910751+MohammedAlhayki@users.noreply.github.com>
2023-04-19 09:30:09 +0300

    Update Courses.class.php

--- app/controllers/Courses.class.php
+++ app/controllers/Courses.class.php
@@ -10,20 +10,23 @@ class Courses extends Controller {
     private $Questions = [];
     private $IsLocked;
 
-    public function __construct()
+
+    public function __construct()   //hdb
     {
-        $this->cmodel = $this->model('course');
+         $this->cmodel = $this->model('course');
+
+
     }
 
 
-    public function index()
+    public function index()  //hdb
     {
+
         $showctc = $this->cmodel->ShowCourses();
 
         $data =[
             'dd' => $showctc
-        ];
-        
+        ]; 
     }
 
     public function details($id)
@@ -36,6 +39,43 @@ class Courses extends Controller {
 
         $this->view('User/course_details', $data);
     }
+
+    public function AddToCart($Course_ID){
+      
+        if($Course_ID != null){
+          
+        if(isLoggedIn()){
+           
+            $data = $this->cmodel->Showdetails($Course_ID);
+            $cmodel = $this->cmodel->fill_Cart($Course_ID);
+            // $this->view('User/course_details', $data);
+            redirect('User/course_details/'.$data->crs_ID);
+
+        }
+        else{
+            echo' you have to log in first or something is wrong';
+        }
+
+        }
+
+    }
+
+
+    public function cart(){
+
+        $cart = $this->cmodel->getCart();
+
+        $data =[
+            'cart' => $cart
+        ]; 
+
+        $this->view('User/cart',$data);
+
+    }
+
+   
+
+
 
 
 
